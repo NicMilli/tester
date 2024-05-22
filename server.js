776cc6b5ab51f1e50a5e8c534aebe8979fb908c2
NicMilli <nicbass2@gmail.com>
2024-05-22 10:10:02 -0700

    feat: start script

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json({ limit: '25mb' }));
+
+app.post('/payments', (req, res) => {
+  console.log('req: ', req);
+  console.log('req.body: ', req?.body);
+
+  res.sendStatus(201);
+})
+
+app.listen(8081);
